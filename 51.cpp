ead09ac5bf540c37ea2780b1406a13412aed854d
Lucas Laborne <lucas.laborne@gmail.com>
2019-06-14 11:15:03 -0300

    Adicionado metodo para printar vetores

--- 51.cpp
+++ 51.cpp
@@ -3,11 +3,30 @@
 #include <string>
 #include <cmath>
 #include <vector>
+#include <ostream>
 
 using namespace std;
 
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> vec);
+
 bool is_prime(int num);
 string to_binary(int n);
+
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> vec)
+{
+    os << "[ ";
+    int size = vec.size();
+    for (int i = 0; i < size; i++)
+    {
+        cout << vec[i];
+        if (i != size - 1)
+            cout << ", ";
+    }
+    os << " ]";
+    return os;
+}
 
 bool is_prime(int num)
 {
@@ -39,6 +58,14 @@ string to_binary(int n)
 
 int main()
 {
+    vector<int> vec;
+    vec.push_back(3);
+    vec.push_back(5);
+    vec.push_back(15);
+    vec.push_back(41);
+    cout << vec << endl;
+
+    return 0;
     for (int i = 0; i < INT_MAX; i++)
     {
         string num = to_string(i);
@@ -49,26 +76,22 @@ int main()
             string bin = to_binary(j);
             int size_bin = bin.size();
             bin = string(size - size_bin, '0').append(bin);
-            int family_size = 0;
             vector<int> primes;
             for (int k = 0; k < 10; k++)
             {
                 string num_to_test = num;
                 for (int d = 0; d < size; d++)
-                {
                     if (bin[d] == '1' && !(d == 0 && k == 0))
                         num_to_test[d] = k + '0';
-                }
                 if (num_to_test == num)
                     continue;
                 int x = atoi(num_to_test.c_str());
                 if (is_prime(x))
                 {
-                    family_size++;
                     primes.push_back(x);
                 }
             }
-            if (family_size == 8)
+            if (primes.size() == 8)
             {
                 cout << num << " - ";
                 for (int &i : primes)
